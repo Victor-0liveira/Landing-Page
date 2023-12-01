71220cc722ba9979b654cd1725c4d8dcd38b5bb5
Victor Oliveira <victorsilva101@hotmail.com>
2023-12-01 18:21:23 -0300

    Fixed selectable cards, added new divs, fixed nav.

--- script.js
+++ script.js
@@ -1,12 +1,39 @@
+// Parte para o Dark Mode da Página
 const themeButton = document.getElementById("theme-button");
 themeButton.addEventListener("click", () => {
   document.body.classList.toggle("dark-theme");
 });
 
+// parte para os cards da parte gift serem selecionáveis
 const cards = document.querySelectorAll(".gift-card");
+
 for (let i = 0; i < cards.length; i++) {
   cards[i].addEventListener("click", () => {
-    cards[i].style.backgroundColor = "#C4A484";
-    cards[i].style.color = "#ff0000";
+    const isModified = cards[i].style.backgroundColor === "rgb(196, 164, 132)";
+
+    if (isModified) {
+      cards[i].style.backgroundColor = "";
+      cards[i].style.color = "";
+    } else {
+      cards[i].style.backgroundColor = "#C4A484";
+      cards[i].style.color = "#ff0000";
+    }
+  });
+}
+
+// parte para os cards da parte new serem selecionáveis
+const cards2 = document.querySelectorAll(".new-card");
+
+for (let i = 0; i < cards2.length; i++) {
+  cards2[i].addEventListener("click", () => {
+    const isModified = cards2[i].style.backgroundColor === "rgb(196, 164, 132)";
+
+    if (isModified) {
+      cards2[i].style.backgroundColor = "";
+      cards2[i].style.color = "";
+    } else {
+      cards2[i].style.backgroundColor = "#C4A484";
+      cards2[i].style.color = "#ff0000";
+    }
   });
 }
